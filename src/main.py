5b2b54c1e5892ed63b4e7d7e6c78e7c5388a49df
ad0maa <adam.tunchay@me.com>
2022-09-19 10:53:04 +1000

    first run on pseudocode written

---
+++ src/main.py
@@ -0,0 +1,27 @@
+# Application Splash Screen 
+
+# Get Users to input names and store in Dictionary with scores set to zero
+
+# Exceptions handling for user input
+
+# generate random player to begin first (only on first load, then winner plays first)
+
+# Print game board and wait wait for user input
+
+# Winner player to select first move
+
+# Check if there is 3 in a row horizontally, vertically or diagonal
+
+# Store move and print game board updated with position of move
+
+# Second player selects position
+
+# repeats above code until player wins
+
+# Update scoreboard with the winner 
+
+# Once game complete, ask if players want to play again
+
+# IF no, present Winner with winner screen 
+
+# End application
