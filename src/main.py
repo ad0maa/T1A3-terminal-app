952627edb015193f31b33feea6d16b6bf58d8d76
ad0maa <adam.tunchay@me.com>
2022-09-21 15:09:18 +1000

    start over, pseudocode written

--- src/main.py
+++ src/main.py
@@ -1,113 +1,33 @@
-# Import
-from unicodedata import name
-# import menuitems
-# import menu
-# import dicts
-import clearing
-import os
-from simple_term_menu import TerminalMenu
-import time
-# clearing.clear()
 
-# Variables
-order_type = ''
+# Welcome Message
 
-# Splash screen
-# print("Hello and Welcome to our Japanese Restaurant")
+# Program Menu (Show Food menu, make an order, exit)
 
+# Create food menu (dictionary)
 
-def ordertype():
-    global order_type
-    order_type = input("Would you like to order Pickup or Delivery?")
-    if order_type.lower() == 'pickup':
-        print('Pickup order selected')
-        order_type = 'pickup'
-        return order_type
+# Print out Food Menu
 
-    elif order_type.lower() == 'delivery':
-        print('Delivery order selected')
-        order_type = 'delivery'
-        return order_type
-    else:
-        print("Please enter either 'pickup' or 'delivery'")
+# Ask 'Are you ready to order?'
 
-# CIL Menu
-
-
-def main():
-    print("Hello and Welcome to our Japanese Restaurant\n")
-    main_menu_items = ["[1] Make an Order", "[2] View Entree Menu", "[3] View Ramen Menu",
-                       "[4] View Main Menu", "[5] View Sushi menu", "[6] View Drinks Menu", "[0] Quit"]
-    terminal_menu = TerminalMenu(main_menu_items)
-    menu_entry_index = terminal_menu.show()
-    # main_menu_exit = False
-
-    print(f"You have selected {main_menu_items[menu_entry_index]}!")
-
-    main_menu = TerminalMenu(
-        menu_entries=main_menu_items,
-        # title=main_menu_title,
-        # clear_screen= True,
-    )
-
-    while not main_menu_exit:
-        main_sel = main_menu.show()
-
-        if main_sel == 0:
-            print("You'd like to make an order!")
-            ordertype()
-            # time.sleep(2)
-        elif main_sel == 1:
-            print("Here is the Entree Menu")
-            print(print_menu(entrees))
-            # time.sleep(2)
-        elif main_sel == 2:
-            print("Here is the Ramen Menu")
-            print(print_menu(ramen))
-            # time.sleep(2)
-        elif main_sel == 3:
-            print("Here is the Main Menu")
-            print(print_menu(mains))
-            # time.sleep(2)
-        elif main_sel == 4:
-            print("Here is the Sushi Menu")
-            print(print_menu(sushi))
-            # time.sleep(2)
-        elif main_sel == 5:
-            print("Here is the Drinks Menu")
-            print(print_menu(drinks))
-            # time.sleep(2)
-        elif main_sel == 6:
-            main_menu_exit = True
-            print("Quit Selected")
-
-
-if __name__ == "__main__":
-    main()
-
-# Get user input on if order is pickup or delivery (build object based on answers)
-
-
-# WORKING CODE HERE
-# ordertype()
-# print(order_type)
-# print(menuitems.cali_roll.__dict__)
-
-# Ask User what part of the menu they would like to display (Entree, Main, Dessert, Drinks)
-
-
-# Ask user if they have allergies, only display items that are appropriate to the user
-
-# Display the menu to user and ask them if they'd like to order something from this menu
-
-# Add menu items to order object
+# Take users order, add to variable 'receipt'
 
 # Ask user if they want to order something else?
 
 # Repeat above steps until user has finished ordering
 
-# If Delivery, add delivery fee to the order
+# Ask if Delivery, add delivery fee to the order
 
 # Produce a receipt and output receipt to a text file
 
+# Exit Application
+
+
+# EXTRAS ONCE BASIC PROGRAM IS RUNNING
+
 # Using datetime, give an estimate of when the order will be ready or delivered
+
+# Show what has been ordered so far, with sub-total price
+
+# Ask user if they have allergies, only display items that are appropriate to the user
+
+# Ask if user is a member and apply discount price to the receipt
