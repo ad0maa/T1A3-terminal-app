9280eba4ba13796a7fc921913316649570c56173
ad0maa <adam.tunchay@me.com>
2022-09-22 21:23:57 +1000

    receipt modified + datetime added

--- src/main.py
+++ src/main.py
@@ -1,10 +1,32 @@
 
 # Welcome Message
 from ast import For
+from datetime import datetime, date
 from prettytable import PrettyTable
-from datetime import date
+import clearing
 
-print('Hello and Welcome to McDoogals')
+
+print('''
+  __  __        ______                        
+ |  \/  |      |  ____|                       
+ | \  / |  ___ | |__  ___    ___              
+ | |\/| | / __||  __|/ _ \  / _ \             
+ | |  | || (__ | |  | (_) || (_) |            
+ |_|__|_| \___||_| _ \___/  \___/             
+ |  __ \       | |(_)                         
+ | |  | |  ___ | | _ __   __ ___  _ __  _   _ 
+ | |  | | / _ \| || |\ \ / // _ \| '__|| | | |
+ | |__| ||  __/| || | \ V /|  __/| |   | |_| |
+ |___/\/  \___||_||_|  \_/  \___||_|    \__, |
+    /  \    _ __   _ __                  __/ |
+   / /\ \  | '_ \ | '_ \                |___/ 
+  / ____ \ | |_) || |_) |                     
+ /_/    \_\| .__/ | .__/                      
+           | |    | |                         
+           |_|    |_|                         
+\n\n''')
+print(input('Press any key to continue'))
+print('Hello and Welcome to McDoogals\n')
 
 # Program Menu (Show Food menu, make an order, exit)
 
@@ -21,7 +43,7 @@ menu = {
 def print_menu():
     print('Here is today\'s menu: ')
     for food, price in menu.items():
-        print (food + '- $' + str(price))
+        print (food, '- $',price)
 
 # Ask 'Are you ready to order?'
 
@@ -36,7 +58,7 @@ total_price = 0
 
 def order_food(order_items, total_price):
     while True:
-        print('What would you like to order today?')
+        print('\nWhat would you like to order today?')
         order_req = input()
         if order_req in menu:
             sub_total = menu[order_req]
@@ -56,15 +78,19 @@ def pickup_delivery(total_price):
     x = input('Is your order delivery or pickup?')
     if x == 'delivery':
         total_price += 7.50
+        print('$7.50 delivery fee has been added to your order.')
         print(f'Your total including delivery is ${total_price}')
-        return
+        delivery = True # THIS NEEDS TO WORK
+        return delivery
     else: print(f'Your total is ${total_price}')
     return
 
 
-
-
 # Produce a receipt and output receipt to a text file
+
+delivery = False
+clearing.clear()
+
 
 def print_receipt():
     global table 
@@ -72,6 +98,9 @@ def print_receipt():
     for k,v in order_items.items():
         table.add_row([k,'$' + str(v)])
     table.add_row(['-'* 8,'-' * 8])
+    if delivery == True:
+        table.add_row(['DELIVERY', '$7.50'])
+        table.add_row(['-'* 8,'-' * 8])
     table.add_row(['TOTAL', '$' + str(total_price)])
     print(table)
     print('Your total bill amount is $', total_price)
@@ -81,10 +110,10 @@ today = date.today()
 
 def file_receipt():
     with open('receipt.txt', 'w') as f:
-        f.write('McDoogals Receipt\n')
-        f.write(f'Date: {today.strftime("%B %d, %Y")}\n')
-
+        f.write('McFoo Receipt\n')
+        f.write(f'Order Placed: {today.strftime("%B %d, %Y")}\n')
         f.write(str(table))
+
 
 # Ask 
 
@@ -110,3 +139,7 @@ total_price = order_food(order_items, total_price)
 pickup_delivery(total_price)
 print_receipt()
 file_receipt()
+
+now = datetime.now()
+now.time()
+print('Your order will be ready at ', now.time())
